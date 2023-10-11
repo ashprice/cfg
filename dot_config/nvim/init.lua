b7f08d6ca508c105ff12ca54522eba92ffb5807d
ashprice <eisleggje@gmail.com>
2023-10-11 05:35:22 +0100

    basic vimtex added

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -19,6 +19,26 @@ require("lazy").setup({
 			end,
 			},
 			{ "nvim-lua/plenary.nvim" },
-		}}
+		}},
+		{ "lervag/vimtex",
+		init = function()
+			vim.g.tex_flavor = 'lualatex'
+			vim.g.vimtex_quickfix_open_on_warning=0
+			vim.g.vimtex_quickfix_autoclose_after_keystrokes=2
+			vim.g.vimtex_quickfix_mode=0
+			vim.g.vimtex_compiler_progname='nvr'
+			vim.g.vimtex_view_general_viewer='zathura'
+			vim.g.vimtex_compiler_latexmk = {
+				continuous = 1,
+				executable = '/usr/bin/latexmk',
+				options = {
+					'-shell-escape'
+				}
+			}
+			vim.g.vimtex_complete_enabled=1
+		end,
+		ft = 'tex'
+		},
 	}
 })
+
