63ea0154810da05bb6453c9aaa10684db63b9bd0
ashprice <eisleggje@gmail.com>
2025-04-05 06:05:27 +0000

    DB didn't like so many connections

--- private_dot_local/bin/scripts/fish/executable_recurrence.fish
+++ private_dot_local/bin/scripts/fish/executable_recurrence.fish
@@ -2,35 +2,22 @@
 
 set parents (task +PARENT _uuids)  # Take the first argument as input
 
-for parent in $parents
-  pueue add -i "set -U children_of_(echo $parent | sd '-' '_') (task rc.gc=0 status:pending parent:$parent due.before:today _uuids)"
-end
-pueue wait && pueue clean
-
-for children in (set -n | rg '^children_of_')
-  if test -n "$$children" && test (count $$children) -gt 1
-    pueue add -i "set -U dues_of_$children (task rc.gc=0 $$children _unique due)"
-  end
-end
-pueue wait && pueue clean
-
 set -U children_to_delete ""
-for children in (set -n | rg '^children_of_')
-  if test -n "$$children" && test (count $$children) -gt 1
-    for dues in (set -n | rg '^dues_of_')
-      if test -n "$$dues" && test (count $$dues) -gt 1
-        for due in $$dues
-          pueue add -i "set -Ua children_to_delete (task rc.gc=0 parent:(echo $children | sd 'children_of_' '' | sd '_' '-') due.before:today due.after:$due _uuids)"
-        end
+for parent in $parents
+  set children (task rc.gc=0 status:pending parent:$parent due.before:today _uuids)
+  if test (count $children) -gt 1
+    set dues (task rc.gc=0 $children _unique due)
+    if test (count $dues) -gt 1
+      for due in $dues
+        set -Ua children_to_delete (task rc.gc=0 parent:$parent "(due.before:today and due.after:$due)" _uuids)
       end
     end
   end
 end
-pueue wait && pueue clean
 
-task rc.bulk=0 rc.confirmation=0 rc.recurrence.confirmation=no $children_to_delete delete
-task rc.bulk=0 rc.confirmation=0 rc.recurrence.confirmation=no status:deleted +CHILD purge
-task rc.bulk=0 rc.confirmation=0 rc.recurrence.confirmation=no status:deleted +PARENT purge
+task rc.bulk=0 rc.confirmation=1 rc.recurrence.confirmation=no $children_to_delete delete
+task rc.bulk=0 rc.confirmation=1 rc.recurrence.confirmation=no status:deleted +CHILD purge
+task rc.bulk=0 rc.confirmation=1 rc.recurrence.confirmation=no status:deleted +PARENT purge
 
 # deleted parents usually aren't useful either
 
