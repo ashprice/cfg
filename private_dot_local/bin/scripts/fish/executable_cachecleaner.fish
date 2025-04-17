8029aa31b91ce1b6fc96ee66295ba07facc0f1a8
ashprice <eisleggje@gmail.com>
2025-04-17 20:30:53 +0100

    Removing empty dirs upsets various utilities and doesn't serve much purpose.

--- private_dot_local/bin/scripts/fish/executable_cachecleaner.fish
+++ private_dot_local/bin/scripts/fish/executable_cachecleaner.fish
@@ -4,13 +4,3 @@ fd -H -t d --changed-before 1week . /home/hearth/.cache/paru/ | sd '(^/([^/]+/){
 
 fd -H -t f --exclude 'fish/' --exclude 'paru/' --changed-before 30days . /home/hearth/.cache/ -X rmz
 
-while true
-  set emptydirs ""
-  set emptydirs (fd -H -t empty -t -d . /home/hearth/.cache)
-  if test -n "$emptydirs"
-    rmz $emptydirs
-  else
-    break
-  end
-end
-
