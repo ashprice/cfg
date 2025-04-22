fda5b5753ea7ada7748e29b43287c9cf5020c99c
ashprice <eisleggje@gmail.com>
2025-04-22 10:59:57 +0100

    Minor fixes

--- dot_config/fish/functions/executable_gf.fish
+++ dot_config/fish/functions/executable_gf.fish
@@ -1,32 +1,32 @@
 function gf
-  argparse -x l,r,g,d,R,D -x C,l,r,g,d,R,D 'h/help' 'l' 'c' 's' 'C' 'r' 'g' 'd' 'R' 'D' -- $argv
+  argparse -x l,r,g,d -x C,l,r,g,d,R,D 'h/help' 'l' 'c' 's' 'C' 'r' 'g' 'd' 'R' 'D' -- $argv
   if set -ql _flag_help
     echo "
-gf - gtrash wrapper
+    gf - gtrash wrapper
 
-General listing:
-  gf -l             => list trash
-  gf -l -c          => list trash from cwd 
-  gf -l -s          => list trash + sort by size 
+    General listing:
+    gf -l             => list trash
+    gf -l -c          => list trash from cwd 
+    gf -l -s          => list trash + sort by size 
 
-Cleanup:
-  gf -C             => prune older than 30days 
-  gf -C -s          => view and select larger items for deletion
+    Cleanup:
+    gf -C             => prune older than 30days 
+    gf -C -s          => view and select larger items for deletion
 
-Restoring:
-  gf -r             => restore 
-  gf -r -c          => restore trashed from cwd 
-  gf -g             => restore in groups 
+    Restoring:
+    gf -r             => restore 
+    gf -r -c          => restore trashed from cwd 
+    gf -g             => restore in groups 
 
-Permanent deletion:
-  gf -d             => permanent delete
-  gf -d -c          => permanent delete from cwd 
+    Permanent deletion:
+    gf -d             => permanent delete
+    gf -d -c          => permanent delete from cwd 
 
-Trashing items:
-  gf                => trash 
-  gf -R             => trash recursively
-  gf -D             => trash directory 
-" 
+    Trashing items:
+    gf                => trash 
+    gf -R             => trash recursively
+    gf -D             => trash directory 
+    " 
     return 0
   end
 
@@ -44,10 +44,10 @@ Trashing items:
       $cmd $argv
     end
   else if set -ql _flag_C
-   if set -ql _flag_s
-      gtrash prune --day 30
-    else
+    if set -ql _flag_s
       gtrash find -S --size-large 500M | fzf --multi | awk -F'\t' '{print $3}' | xargs -d '\n' -o -r gtrash rm
+    else
+      gtrash prune --day 30
     end
   else if set -ql _flag_r
     set cmd gtrash find -S
@@ -63,18 +63,12 @@ Trashing items:
       set cmd $cmd --cwd
     end
     $cmd | fzf --multi | awk -F'\t' '{print $3}' | xargs -d '\n' -o -r gtrash rm
+  else if set -ql _flag_R
+    gtrash put --recursive $argv
+  else if set -ql _flag_D
+    gtrash put --dir $argv
   else
-    if test -z "$argv"
-      echo "Expected at least one argument"
-    else
-      set cmd gtrash put --rm-mode
-      if set -ql _flag_R
-        $cmd --recursive
-      else if set -ql _flag_D
-        $cmd --dir
-      end
-      $cmd $argv
-    end
+    gtrash put $argv
   end
 end
 
