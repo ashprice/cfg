f34aee46b6ccc0ed64906d2dd9af56b5f2a267ea
ashprice <eisleggje@gmail.com>
2025-04-23 17:18:27 +0100

    improved speed of tw recurrence script

--- private_dot_local/bin/scripts/fish/executable_recurrence.fish
+++ private_dot_local/bin/scripts/fish/executable_recurrence.fish
@@ -1,25 +1,42 @@
 #!/usr/bin/env fish
 
-set parents (task +PARENT _uuids)  # Take the first argument as input
+# this is all quite slow. 
+# in the absence of taskwarrior profiling data I don't really know how to speed it up 
+# maybe working with the JSON in julia would be better
 
-set -U children_to_delete ""
+set parents (task +PARENT _uuids)  
+
+set children_to_delete ""
 for parent in $parents
-  set children (task rc.gc=0 status:pending parent:$parent due.before:today _uuids)
+  set children (task '(status:pending or status:waiting)' parent:$parent due.before:today _uuids)
   if test (count $children) -gt 1
-    set dues (task rc.gc=0 $children _unique due)
+    set dues (task $children _unique due)
     if test (count $dues) -gt 1
-      for due in $dues
-        set -Ua children_to_delete (task rc.gc=0 parent:$parent "(due.before:today and due.after:$due)" _uuids)
-      end
+      set due (printf "%s\n" | sort -n | head -n 1)
+      set children_to_delete (task '(status:pending or status:waiting)' parent:$parent "(due.before:today and due.after:$due)" _uuids)
+      task rc.bulk=0 rc.confirmation=0 rc.recurrence.confirmation=no $children_to_delete delete
+      set -e due 
     end
   end
 end
 
-task rc.bulk=0 rc.confirmation=1 rc.recurrence.confirmation=no $children_to_delete delete
-task rc.bulk=0 rc.confirmation=1 rc.recurrence.confirmation=no status:deleted +CHILD purge
-task rc.bulk=0 rc.confirmation=1 rc.recurrence.confirmation=no status:deleted +PARENT purge
+set recurs (task _unique recur)
+for span in $recurs
+  set 30spans (task calc "$span*30" | sd '[a-zA-Z](\d+)[a-zA-Z]' '$1')
+  set until 0
 
-# deleted parents usually aren't useful either
+  if test $30spans -lt 90
+    set until 90
+  else if test $30spans -gt 90
+    set until (task calc "$span*3" | sd '[a-zA-Z](\d+)[a-zA-Z]' '$1') 
+  end
 
+  task rc.bulk=0 rc.confirmation=0 rc.recurrence.confirmation=0 '(status:recurring or status:pending or status:waiting)' recur:$span mod until:today+{$until}d
+  set -e until
+end
+     
+
+#task rc.bulk=0 rc.confirmation=0 rc.recurrence.confirmation=no status:deleted +CHILD purge
+#task rc.bulk=0 rc.confirmation=0 rc.recurrence.confirmation=no status:deleted +PARENT purge
 
 
