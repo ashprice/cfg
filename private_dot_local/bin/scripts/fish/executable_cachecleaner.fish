9d10e40c8a4cc9d89fc33a55a25052bd0d2c5122
ashprice <eisleggje@gmail.com>
2025-05-10 13:41:24 +0100

    Added PSA

--- private_dot_local/bin/scripts/fish/executable_cachecleaner.fish
+++ private_dot_local/bin/scripts/fish/executable_cachecleaner.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 
+# nb: please don't use this, I was doing some testing and it returned `/` as a directory to delete
+
 fd -H -t d --changed-before 1week . /home/hearth/.cache/paru/ | sd '(^/([^/]+/){6}).*' '$1' | sort -u | xargs -I_ rmz _
 
 fd -H -t f --exclude 'fish/' --exclude 'paru/' --changed-before 30days . /home/hearth/.cache/ -X rmz
