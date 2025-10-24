e16731b36d75ced53d8431cfc0e97732dca0b775
ashprice <eisleggje@gmail.com>
2025-10-24 21:33:39 +0100

    Some wezterm changes unhelpfully squashed into a single commit

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,16 +6,20 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Kanagawa (Gogh)'
-config.tab_bar_at_bottom = true
+config.scrollback_lines = 1000000
+config.color_scheme = 'Monokai Remastered'
+config.tab_bar_at_bottom = false
 config.window_frame = {
   font_size = 14.0,
 }
-config.window_background_opacity = 0.8
-config.text_background_opacity = 0.65
+config.window_background_opacity = 0.9
+config.text_background_opacity = 0.8
 config.front_end = 'WebGpu'
 config.enable_wayland = true
-config.font = wezterm.font_with_fallback { 'Sarasa Term J', 'Sarasa Term K', 'Sarasa Term CL', 'Sarasa Term TC', 'Sarasa Term SC' }
+config.font = wezterm.font_with_fallback { 'Sarasa Mono J', 'Sarasa Mono K', 'Sarasa Mono CL', 'Sarasa Mono TC', 'Sarasa Mono SC' }
+config.font_size = 18
+config.detect_password_input = true
+config.ime_preedit_rendering = "Builtin"
 config.launch_menu = {
   {
     label = 'btm',
@@ -23,7 +27,6 @@ config.launch_menu = {
   },
 }
 config.use_ime = true
-config.xim_im_name = 'ibus-daemon -drx'
 config.keys = {
   { key = 'c', mods = 'ALT', action = wezterm.action.CopyTo 'Clipboard' },
   { key = 'v', mods = 'ALT', action = wezterm.action.PasteFrom 'Clipboard' },
@@ -34,10 +37,11 @@ config.keys = {
   { key = 'w', mods = 'ALT', action = wezterm.action.CloseCurrentTab{confirm=true} },
   { key = 'k', mods = 'ALT', action = wezterm.action.ScrollByLine(-1) },
   { key = 'j', mods = 'ALT', action = wezterm.action.ScrollByLine(1) },
+  { key = 'Space', mods = 'CTRL', action = wezterm.action.DisableDefaultAssignment },
+  { key = 'Space', mods = 'CTRL|SHIFT', action = wezterm.action.DisableDefaultAssignment },
 }
 for i = 1, 9 do
   table.insert(config.keys, { key = tostring(i), mods = 'ALT', action = wezterm.action.ActivateTab(i - 1), })
 end
-config.font_size = 24.0
 
 return config
